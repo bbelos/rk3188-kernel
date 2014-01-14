5dc5a4e2dac3456fdb7db50716c613b76ebe4a02
WengBaijie <wengbaijie@t-chip.com.cn>
2014-01-14 19:22:41 +0800

    Tchip->Gsensor:调整重力传感器方向[tr7088]

--- drivers/input/gsensor/stk831x.c
+++ drivers/input/gsensor/stk831x.c
@@ -208,7 +208,7 @@ static int32_t stk_get_ic_content(struct stk831x_data *stk);
 static int STK831x_SetOffset(char buf[]);
 static void stk_handle_first_en(struct stk831x_data *stk);
 
-#if defined(CONFIG_TCHIP_MACH_TR1088)
+#if defined(CONFIG_TCHIP_MACH_TR1088) || defined(CONFIG_TCHIP_MACH_TR7088)
 static int gsensor_direct_x = 1;
 static int gsensor_direct_y = 1;
 static int gsensor_direct_z = 0;
