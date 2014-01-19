3b1bf5ec8536bdfe148324cc15a9f16df498bdcd
hlq <hlq@t-chip.com.cn>
2014-01-19 15:44:31 +0800

    TCHIP:ARM1.2G时提高0.025V电压[TR1088]

--- arch/arm/mach-rk3188/board-rk3188-ds1006h.c
+++ arch/arm/mach-rk3188/board-rk3188-ds1006h.c
@@ -2717,7 +2717,7 @@ static struct cpufreq_frequency_table dvfs_arm_table_volt_level2[] = {
         {.frequency = 504 * 1000,       .index = 950 * 1000},
         {.frequency = 816 * 1000,       .index = 1000 * 1000},
         {.frequency = 1008 * 1000,      .index = 1100 * 1000},
-        {.frequency = 1200 * 1000,      .index = 1200 * 1000},
+        {.frequency = 1200 * 1000,      .index = 1225 * 1000},
         {.frequency = 1416 * 1000,      .index = 1275 * 1000},
 #if !defined(CONFIG_TCHIP_MACH_TR1088)
         {.frequency = 1608 * 1000,      .index = 1350 * 1000},
