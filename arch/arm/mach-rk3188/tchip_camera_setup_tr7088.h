483907effec0e35eec295fb3993a0a8b32f0340d
lhg <lhg@t-chip.com.cn>
2014-02-20 15:56:47 +0800

    Tchip->camera:添加后置sp0838和优化sp0838拍照效果【tr7078】

--- arch/arm/mach-rk3188/tchip_camera_setup_tr7088.h
+++ arch/arm/mach-rk3188/tchip_camera_setup_tr7088.h
@@ -20,6 +20,26 @@ static struct rkcamera_platform_data new_camera[] = {
                         CONS(RK29_CAM_SENSOR_SP0838,_I2C_ADDR),
                         0,
                         24),
+
+    new_camera_device_ex(RK29_CAM_SENSOR_SP0838,
+                        back,
+                        180,
+                        INVALID_VALUE,
+                        INVALID_VALUE,
+                        INVALID_VALUE,
+                        INVALID_VALUE,
+                        RK30_PIN3_PB4,
+                        (CONS(RK29_CAM_SENSOR_SP0838,_PWRDN_ACTIVE)&0x10)|0x01,
+                        0,
+                        CONS(RK29_CAM_SENSOR_SP0838,_FULL_RESOLUTION),
+                        0x00,
+                        3,
+                        100000,
+                        CONS(RK29_CAM_SENSOR_SP0838,_I2C_ADDR),
+                        0,
+                        24),
+    
+
     new_camera_device_end
 };
 #endif
