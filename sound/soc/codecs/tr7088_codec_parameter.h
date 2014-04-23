5b6fd42716ec3ebc8906daa040b3aacc4a1b5230
WengBaijie <wengbaijie@t-chip.com.cn>
2014-04-23 16:20:33 +0800

    Tchip->Codec:分离codec参数[tr7088]

---
+++ sound/soc/codecs/tr7088_codec_parameter.h
@@ -0,0 +1,9 @@
+#ifndef __TR7088_CODEC_PARAMETER_H__
+#define __TR7088_CODEC_PARAMETER_H__
+
+#define  SPKOUT_VOLUME    22 //0~31
+#define  HPOUT_VOLUME     24 //0~31
+#define   CAPTURE_VOL    18//0-31
+
+#endif
+
