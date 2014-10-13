78c70aff1fbaf78ec14f8304623bc926472a0934
RuanJG <rjg@t-chip.com.cn>
2014-10-13 10:35:19 +0800

    Tchip->power: tr101q 配置选上CONFIG_ARCH_RK3188T,并修改arm最高电压值

--- arch/arm/mach-rk3188/board-rk3188-ds1006h.c
+++ arch/arm/mach-rk3188/board-rk3188-ds1006h.c
@@ -3690,6 +3690,20 @@ static struct cpufreq_frequency_table dvfs_arm_table_volt_level3[] = {
         {.frequency = CPUFREQ_TABLE_END},
 };
 
+#elif defined(CONFIG_TCHIP_MACH_TR101Q)
+static struct cpufreq_frequency_table dvfs_arm_table_volt_level2[] = {
+        {.frequency = 312 * 1000,       .index = 950 * 1000},
+        {.frequency = 504 * 1000,       .index = 950 * 1000},
+        {.frequency = 816 * 1000,       .index = 1025 * 1000},
+        {.frequency = 1008 * 1000,      .index = 1100 * 1000},
+        {.frequency = 1200 * 1000,      .index = 1225 * 1000},
+        {.frequency = 1416 * 1000,      .index = 1300 * 1000},
+		#ifndef CONFIG_ARCH_RK3188T
+        {.frequency = 1608 * 1000,      .index = 1350 * 1000},
+		#endif
+        {.frequency = CPUFREQ_TABLE_END},
+};
+
 #else
 static struct cpufreq_frequency_table dvfs_arm_table_volt_level2[] = {
         {.frequency = 312 * 1000,       .index = 950 * 1000},
