593c796dfc0d746bc64a3966a8619c5370128e4e
lipf <lpf@t-chip.com.cn>
2014-10-31 19:56:38 +0800

    Tchip：添加对MT7601的支持【TR785V21】

--- arch/arm/mach-rk3188/board-rk3188-ds1006h-sdmmc-config.c
+++ arch/arm/mach-rk3188/board-rk3188-ds1006h-sdmmc-config.c
@@ -73,8 +73,11 @@ int rk31sdk_get_sdmmc0_pin_io_voltage(void)
 #if defined(CONFIG_RTL8192CU) || defined(CONFIG_RTL8188EU) || defined(CONFIG_RTL8723AU) \
 	|| defined(CONFIG_RTL8192DU)
     #define RK30SDK_WIFI_GPIO_POWER_N               RK30_PIN3_PD0            
-    #define RK30SDK_WIFI_GPIO_POWER_ENABLE_VALUE    GPIO_LOW//GPIO_HIGH        
-    
+    #define RK30SDK_WIFI_GPIO_POWER_ENABLE_VALUE    GPIO_LOW//GPIO_HIGH
+#elif defined (CONFIG_MT7601)
+    #define RK30SDK_WIFI_GPIO_POWER_N               RK30_PIN3_PD0//RK30_PIN1_PB3//WIFI_PWN
+    #define RK30SDK_WIFI_GPIO_POWER_ENABLE_VALUE    GPIO_HIGH         
+        
 #elif defined(CONFIG_BCM4329) || defined(CONFIG_BCM4319) || defined(CONFIG_RKWIFI) \
 	|| defined(CONFIG_RTL8189ES) || defined(CONFIG_RTL8723BS)
 #if defined(CONFIG_TCHIP_MACH_TR1088) || defined(CONFIG_TCHIP_MACH_TR838)
---
+++ drivers/net/wireless/rkusbwifi/mt7601/DPA_MT7601U_ANDROID_JB_3.0.0.6_20130830_SOFTAP/cp_ko.sh
@@ -0,0 +1,2 @@
+cp PREALLOC/os/linux/mtprealloc7601Usta.ko /proj/mtk05650/share/mt7601_softap/
+cp os/linux/mt7601Usta.ko /proj/mtk05650/share/mt7601_softap/
